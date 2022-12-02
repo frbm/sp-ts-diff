bbca340b3f363c9fb3512cbd6383fff0e3fee456
frbm <frbm@pm.me>
2022-12-02 20:33:12 +0100

    add metrics notebook

--- main_train.py
+++ main_train.py
@@ -1,4 +1,4 @@
-from torch.utils.data import DataLoader
+from torch.qutils.data import DataLoader
 from model import *
 from parameters import *
 from train import *
@@ -12,7 +12,7 @@ print('Number of trainable parameters: ', sum(p.numel() for p in model.parameter
 
 optimizer = torch.optim.Adam(model.parameters(), lr=params.learning_rate)
 
-tr_dataset = TemperatureDataset('./data/df_train.csv', series_length=300, max_date=7700)
+tr_dataset = TemperatureDataset('./data/df_train.csv', series_length=3000, max_date=7700)
 tr_dataloader = DataLoader(tr_dataset, batch_size=16, shuffle=True)
 
 trainer = DiffWaveTrain(model, tr_dataloader, optimizer, params, fp16=False)
